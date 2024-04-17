01ad4d5c65cd54d15fafe66d321b429bdd53d386
totimehjames123 <totimehjames123@gmail.com>
2024-04-17 11:14:13 +0000

    added redux-persist

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counter.jsx'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import counterReducer from './counter.jsx';
+import { nonSerializableMiddleware } from './reduxMiddleware.js'; // Import the middleware
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: persistedReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(nonSerializableMiddleware),
+});
+
+export const persistor = persistStore(store);
